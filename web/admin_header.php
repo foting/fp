3cfbbba0316c2a7b6e9eae99c86644341aa86c11
David Eklov <david.eklov@it.uu.se>
2012-08-11 19:23:09 +0200

    Removed admin dashboard

--- web/admin_header.php
+++ web/admin_header.php
@@ -1,6 +1,5 @@
 <p>FridayPub Admin's Area</p>
 
-<p><a href="admin_dashboard.php">dashboard</a></p>
-<p><a href="admin_useradd.php">add user</a></p>
+<a href="admin_useradd.php">add user</a>
 
 <hr>
