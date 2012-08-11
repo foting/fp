279bd8eec7209efa8386190ee30bfe81d67a7df1
David Eklov <david.eklov@it.uu.se>
2012-08-11 03:47:04 +0200

    Added line (<hr>) below the headers.

--- web/admin_header.php
+++ web/admin_header.php
@@ -1,3 +1,6 @@
 <p>FridayPub Admin's Area</p>
 
 <p><a href="admin_dashboard.php">dashboard</a></p>
+<p><a href="admin_useradd.php">add user</a></p>
+
+<hr>
--- web/user_header.php
+++ web/user_header.php
@@ -2,3 +2,5 @@
 
 <p><a href="user_purchase.php">purchase</a></p>
 <p><a href="user_iou.php">iou</a></p>
+
+<hr>
