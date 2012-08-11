79c8d795b09be54875c51b1f96f442070e77e976
David Eklov <david.eklov@it.uu.se>
2012-08-11 22:07:55 +0200

    Check that user supplied login information.

--- web/login.php
+++ web/login.php
@@ -7,30 +7,39 @@
             $posted_name = $_POST["user_name"];
             $posted_passwd = $_POST["password"];
 
+            if (!$posted_name) {
+                die("Please enter your user name.");
+            }
+            if (!$posted_passwd) {
+                die("Please enter password user name.");
+            }
+
             /* Check password and  credentials */
-            include_once "fpdb.php";
             try {
-                $fpdb = new FPDB();
-                $fpdb_result = $fpdb->user_get($posted_name);
+                include_once "fpdb.php";
+                $db = new FPDB();
+                $user = $db->user_get($posted_name);
             } catch (FPDBException $e) {
                 die($e->getMessage());
             }
-            extract($fpdb_result);
 
-            if ($password != md5($posted_passwd)) {
-                /* XXX Handle this. Go back to login page? */
-                die("Wrong password");
+            if (!$user) {
+                die("Unknown user name.");
+            }
+
+            if ($user["password"] != md5($posted_passwd)) {
+                die("Forgot your password? Sorry your fucked.");
             }
 
             /* XXX We are losing session information */
             session_start();
-            $_SESSION["user_id"] = $user_id;
-            $_SESSION["first_name"] = $first_name;
-            $_SESSION["last_name"] = $last_name;
-            $_SESSION["credentials"] = $credentials;
+            $_SESSION["user_id"] = $user["user_id"];
+            $_SESSION["first_name"] = $user["first_name"];
+            $_SESSION["last_name"] = $user["last_name"];
+            $_SESSION["credentials"] = $user["credentials"];
 
             include_once "credentials.php";
-            switch ($credentials) {
+            switch ($user["credentials"]) {
                 case CRED_ADMIN:
                     include_once "admin_welcome.php";
                     break;
