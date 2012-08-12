4e21951700044b0e17515c8d8896fdeb2f4ed9ca
David Eklov <david.eklov@it.uu.se>
2012-08-12 14:43:16 +0200

    Removed unecessary html tags.

--- web/login.php
+++ web/login.php
@@ -1,49 +1,41 @@
-<html>
-    <head>
-        <title>FridayPub User's Area</title>
-    </head>
-    <body>
-        <?php
-            session_start() || die("Failed to start session.");
+<?php
+    session_start() || die("Failed to start session.");
 
-            $username = $_POST["username"];
-            $password = $_POST["password"];
+    $username = htmlspecialchars($_POST["username"]);
+    $password = htmlspecialchars($_POST["password"]);
 
-            /* Check password and  credentials */
-            try {
-                include_once "fpdb.php";
-                $db = new FPDB();
-                $user = $db->user_get($username);
-            } catch (FPDBException $e) {
-                die($e->getMessage());
-            }
+    include_once "fpdb.php";
+    try {
+        $db = new FPDB();
+        $user = $db->user_get($username);
+    } catch (FPDBException $e) {
+        die($e->getMessage());
+    }
 
-            if (!$user) {
-                die("Unknown username: " . $username);
-            }
+    if (!$user) {
+        die("Unknown username: " . $username);
+    }
 
-            if ($user["password"] != md5($password)) {
-                die("Did you forget your password? Sorry your fucked.");
-            }
+    if ($user["password"] != md5($password)) {
+        die("Did you forget your password? Sorry your fucked.");
+    }
 
-            $_SESSION["username"] = $username;
-            $_SESSION["user_id"] = $user["user_id"];
-            $_SESSION["first_name"] = $user["first_name"];
-            $_SESSION["last_name"] = $user["last_name"];
-            $_SESSION["credentials"] = $user["credentials"];
+    $_SESSION["username"] = $username;
+    $_SESSION["user_id"] = $user["user_id"];
+    $_SESSION["first_name"] = $user["first_name"];
+    $_SESSION["last_name"] = $user["last_name"];
+    $_SESSION["credentials"] = $user["credentials"];
 
-            include_once "credentials.php";
-            switch ($user["credentials"]) {
-                case CRED_ADMIN:
-                    include_once "admin_welcome.php";
-                    break;
-                case CRED_USER:
-                    include_once "user_welcome.php";
-                    break;
-                default:
-                    /* XXX Handle error */
-                    break;
-            }
-        ?> 
-    </body>
-</html>
+    include_once "credentials.php";
+    switch ($user["credentials"]) {
+        case CRED_ADMIN:
+            header("location: admin_welcome.php");
+            break;
+        case CRED_USER:
+            header("location: user_welcome.php");
+            break;
+        default:
+            /* XXX Handle error */
+            break;
+    }
+?> 
