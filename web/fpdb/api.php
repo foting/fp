739bef7fe397e848d70f2b0a0c12e1e8d4c459a9
David Eklov <david.eklov@it.uu.se>
2012-08-19 19:14:05 +0200

    Made FPDB API stateless and added purchase_{get,get_all,append} actions.

--- web/fpdb/api.php
+++ web/fpdb/api.php
@@ -2,24 +2,20 @@
     include_once "../fpdb/fpdb.php";
     include_once "../include/credentials.php";
 
-    define("DEBUG", True);
-    
-    define("ERROR_USERNAME", 1);
-    define("ERROR_PASSWORD", 2);
-    define("ERROR_CRED",     3);
-    define("ERROR_SESSION",  4);
-    define("ERROR_TIMEOUT",  5);
-    define("ERROR_DATABASE", 6);
-    define("ERROR_ACTION",   7);
+    define("ERROR_USERNAME",  1);
+    define("ERROR_PASSWORD",  2);
+    define("ERROR_CRED",      3);
+    define("ERROR_DATABASE",  4);
+    define("ERROR_ACTION",    5);
+    define("ERROR_ARGUMENTS", 6);
 
     $error_strings = array(
-            ERROR_USERNAME => "Username not found",
-            ERROR_PASSWORD => "Wrong password (your fucked)",
-            ERROR_CRED     => "Not enough credentails",
-            ERROR_SESSION  => "Failed to start session",
-            ERROR_TIMEOUT  => "Session timed out",
-            ERROR_DATABASE => "Database error",
-            ERROR_ACTION   => "Unknown action requested",
+            ERROR_USERNAME  => "Username not found",
+            ERROR_PASSWORD  => "Wrong password (your fucked)",
+            ERROR_CRED      => "Not enough credentails",
+            ERROR_DATABASE  => "Database error",
+            ERROR_ACTION    => "Unknown action requested",
+            ERROR_ARGUMENTS => "Wrong number or type of arguments",
     );
 
     class API_Reply
@@ -32,142 +28,165 @@
             $this->type = $type;
             $this->payload = $payload;
         }
-
-        public function encode()
-        {
-            if (function_exists(json_encode)) {
-                return json_encode($this);
-            } else {
-                $json = "{\"type\" : \"$this->type\", \"payload\" : [";
-                foreach ($this->payload as $record) {
-                    $json .= "{";
-                    foreach ($record as $key => $val) {
-                        $json .= "\"$key\" : \"$val\",";
-                    }
-                    $json .= "},";
-                }
-                $json .= "]}";
-                return $json;
-            }
-        }
     }
 
-    function debug_output($msg, $var)
-    {
-        if (DEBUG) {
-            echo "$msg: ";
-            if ($var) {
-                print_r($var);
-            } else {
-                echo "empty";
-            }
-            echo "</br>";
-        }
-    }
-
+    /*
+     * Helper functions
+     */ 
     function return_error($code)
     {
         global $error_strings;
 
-        $jres = new API_Reply("error", array("code" => $code, "message" => $error_strings[$code]));
-        echo $jres->encode();
+        $payload = array(array("code" => $code, "msg" => $error_strings[$code]));
+
+        $reply = new API_Reply("error", $payload);
+        echo my_json_encode($reply);
         exit(-1);
     }
 
-    function check_credentials($cred)
+    function check_credentials($have, $need)
     {
-        if ($_SESSION["credentials"] > $cred) {
+        if ($have > $need) {
             return_error(ERROR_CRED);
         }
     }
 
+    /* Not all servers at uni have json_encode, hence this wrapper */
+    function my_json_encode($reply)
+    {
+        if (function_exists(json_encode)) {
+            return json_encode($reply);
+        } else {
+            $json = "{\"type\" : \"$reply->type\", \"payload\" : [";
+            foreach ($reply->payload as $record) {
+                $json .= "{";
+                foreach ($record as $key => $val) {
+                    $json .= "\"$key\" : \"$val\",";
+                }
+                $json .= "},";
+            }
+            $json .= "]}";
+            return $json;
+        }
+    }
+
     /*
-     * Functions to handle requests
+     * Functions to handle action requests
      */
-    function api_login($db)
+    function action_inventory_get($db, $user_id)
     {
-        $username = $_GET["username"];
-        $password = $_GET["password"];
- 
-        $qres = $db->user_get($username)->next();
-
-        if (!$qres) {
-            return_error(ERROR_USERNAME);
-        }
-
-        if ($qres["password"] == md5($password)) {
-            $_SESSION["active"] = True;
-            $_SESSION["user_id"] = $qres["user_id"];
-            $_SESSION["credentials"] = $qres["credentials"];
-        } else {
-            return_error(ERROR_PASSWORD);
-        }
-
-        $jres = new API_Reply("login");
-        echo $jres->encode();
-    }
-
-    function api_inventory_get($db)
-    {
-        check_credentials(CRED_ADMIN);
         $qres = $db->inventory_get_all()->get_array();
-        $jres = new API_Reply("inventory_get", $qres);
-        echo $jres->encode();
+        return new API_Reply("inventory_get", $qres);
     }
 
-    function api_iou_get($db)
+    function action_purchase_get($db, $user_id)
     {
-        check_credentials(CRED_USER);
-        $user_id = $_SESSION["user_id"];
+        $qres = $db->purchase_get($user_id)->get_array();
+        return new API_Reply("inventory_get", $qres);
+    }
+
+    function action_purchase_get_all($db, $user_id)
+    {
+        $qres = $db->purchase_get_all()->get_array();
+        return new API_Reply("inventory_get", $qres);
+    }
+
+    function action_purchase_append($db, $user_id)
+    {
+        $beer_id = $_GET["beer_id"];
+        if (!$beer_id) {
+            return_error(ERROR_ARGUMENTS);
+        }
+        $db->purchase_append($user_id, $beer_id);
+        return new API_Reply("empty");
+    }
+
+    function action_iou_get($db, $user_id)
+    {
         $qres = $db->iou_get($user_id)->get_array();
-        $jres = new API_Reply("iou_get", $qres);
-        echo $jres->encode();
+        return new API_Reply("iou_get", $qres);
     }
 
-    function api_iou_get_all($db)
+    function action_iou_get_all($db, $user_id)
     {
-        check_credentials(CRED_ADMIN);
         $qres = $db->iou_get_all()->get_array();
-        $jres = new API_Reply("iou_get_all", $qres);
-        echo $jres->encode();
+        return new API_Reply("iou_get_all", $qres);
     }
 
-    if (!session_start()) {
-        return_error(ERROR_SESSION);
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+
+    if (!$username || !$password) {
+        return_error(ERROR_ARGUMENTS);
     }
-
-    $action = $_GET["action"];
-    debug_output("action", $action);
-
-    if ($action != "login" and !isset($_SESSION["active"])) {
-        return_error(ERROR_TIMEOUT);
-    }
-
+    
+    /* Check username and password */
     try {
-        $cred = $_SESSION["credentials"];
-        if ($action == "login" or $cred == CRED_USER) {
-            $db = new FPDB_User();
-        } else {
+        $db = new FPDB_User();
+        $qres = $db->user_get($username)->next();
+    } catch (FPDB_Exception $e) {
+        return_error(ERROR_DATABASE);
+    }
+
+    if (!$qres) {
+        return_error(ERROR_USERNAME);
+    }
+
+    if ($qres["password"] != md5($password)) {
+        return_error(ERROR_PASSWORD);
+    }
+
+    $user = $qres["user_id"];
+    $cred = $qres["credentials"];
+
+    /* Reconnect to database with correct credentials */
+    try {
+        if ($credentials == CRED_ADMIN) {
+            unset($db); /* Close connection */
             $db = new FPDB_Admin();
         }
     } catch (FPDB_Exception $e) {
         return_error(ERROR_DATABASE);
     }
 
+    $action = $_GET["action"];
+    if (!$action) {
+        return_error(ERROR_ARGUMENTS);
+    }
+
+    /* Perform requested action */
     try {
         switch ($action) {
-            case "login":
-                api_login($db);
-                break;
             case "inventory_get":
-                api_inventory_get($db);
+                check_credentials($cred, CRED_USER);
+                $reply = action_inventory_get($db, $user);
                 break;
+
+            case "purchase_get":
+                check_credentials($cred, CRED_USER);
+                $reply = action_purchase_get($db, $user);
+                break;
+
+            case "purchase_get_all":
+                check_credentials($cred, CRED_ADMIN);
+                $reply = action_purchase_get_all($db, $user);
+                break;
+
+            case "purchase_append":
+                check_credentials($cred, CRED_USER);
+                $reply = action_purchase_append($db, $user);
+                break;
+
             case "iou_get":
-                api_iou_get($db);
+                check_credentials($cred, CRED_USER);
+                $reply = action_iou_get($db, $user);
                 break;
+
             case "iou_get_all":
-                api_iou_get_all($db);
+                check_credentials($cred, CRED_ADMIN);
+                $reply = action_iou_get_all($db, $user);
                 break;
+
             default:
                 return_error(ERROR_ACTION);
                 break;
@@ -175,4 +194,6 @@
     } catch (FPDB_Exception $e) {
         return_error(ERROR_DATABASE);
     }
+
+    echo my_json_encode($reply);
 ?>
