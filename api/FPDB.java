81f032f3561a3e6eeb156d7cdf8b3fba904a6d13
David Eklov <david.eklov@it.uu.se>
2012-08-19 22:23:04 +0200

    Rewrite of java side API

--- api/FPDB.java
+++ api/FPDB.java
@@ -1,107 +1,170 @@
 import java.util.*;
 import java.io.*; 
+import java.net.*;
 import org.json.simple.*;
 
-class ErrorCodes {
-    public static final int ERROR_USERNAME = 1;
-    public static final int ERROR_PASSWORD = 2;
-    public static final int ERROR_CRED     = 3;
-    public static final int ERROR_SESSION  = 4;
-    public static final int ERROR_TIMEOUT  = 5;
-    public static final int ERROR_DATABASE = 6;
-    public static final int ERROR_ACTION   = 7;
-
-    public static final int ERROR_CLIENT   = 100;
-}
 
 class FPDBException extends Exception
 {
-    public int code;
-    public String msg;
-
-    public FPDBException(int code, String msg)
-    {
-        this.code = code;
-        this.msg = msg;
-    }
-
     public FPDBException(String msg)
     {
-        System.out.println(msg);
-        this.code = ErrorCodes.ERROR_CLIENT;
-        this.msg = msg;
-    }
-
-    public String getMessage()
-    {
-        return "Error: " + code + ": " + msg;
+        super(msg);
     }
 }
 
-class FPDBReply implements Iterable<Map<String, String>>
+
+interface FBDPReplyFactory<T>
+{  
+    T create(Map<String, String> map) throws FPDBException;
+} 
+
+
+class FPDBReply<T>
 {
-    public String type;
-    public List<Map<String, String>> payload;
+    LinkedList<T> payload;
 
-    public FPDBReply(JSONObject jobj) throws FPDBException
+    public FPDBReply(String jstr, FBDPReplyFactory<T> factory) throws FPDBException
     {
-        type = (String)jobj.get("type");
+        JSONObject jobj = (JSONObject)JSONValue.parse(jstr);
+        if (jobj == null) {
+            throw new FPDBException("JSON Parser error");
+        }
+
+        String type = (String)jobj.get("type");
         if (type == null) {
-            throw new FPDBException("Reply has no type attribute");
+            throw new FPDBException("JSON no type attribute");
         }
 
-        payload = (List<Map<String, String>>)jobj.get("payload");
-        if (payload == null) {
-            throw new FPDBException("Reply has no payload attribute");
+        List<Map<String, String>> payload_map = (List<Map<String, String>>)jobj.get("payload");
+        if (payload_map == null) {
+            throw new FPDBException("JSON no payload attribute");
         }
-    }
 
-    /* Iterator interface */
-    public Iterator<Map<String, String>> iterator()
-    {
-        return payload.iterator();
+        payload = new LinkedList<T>();
+        for (Map<String, String> i : payload_map) {
+            payload.add(factory.create(i));
+        }
     }
 }
+
+
+class _FPDBReplyInventory
+{
+    public String name;
+    public int    beer_id;
+    public int    count;
+    public float  price;
+
+    public _FPDBReplyInventory(Map<String, String> map) throws FPDBException
+    {
+        name = map.get("name");
+        beer_id = Integer.parseInt(map.get("beer_id"));
+        count = Integer.parseInt(map.get("count"));
+        price = Float.parseFloat(map.get("price"));
+    }
+
+}
+
+class _FPDBReplyIOU
+{
+    public String username;
+    public String first_name;
+    public String last_name;
+    public float  assets;
+
+    public _FPDBReplyIOU(Map<String, String> map) throws FPDBException
+    {
+        username = map.get("username");
+        first_name = map.get("first_name");
+        last_name = map.get("last_name");
+        assets = Float.parseFloat(map.get("assets"));
+    }
+}
+
+/*
+ * Factory classes
+ */
+class FPDBReplyInventory_Factory implements FBDPReplyFactory<_FPDBReplyInventory>
+{
+    public _FPDBReplyInventory create(Map<String, String> map) throws FPDBException
+    {
+        return new _FPDBReplyInventory(map);
+    }
+}
+
+class FPDBReplyIOU_Factory implements FBDPReplyFactory<_FPDBReplyIOU>
+{
+    public _FPDBReplyIOU create(Map<String, String> map) throws FPDBException
+    {
+        return new _FPDBReplyIOU(map);
+    }
+}
+
+/*
+ * Essentially typedefs
+ */
+class FPDBReplyInventory extends FPDBReply<_FPDBReplyInventory>
+{
+    public FPDBReplyInventory(String jstr) throws FPDBException
+    {
+        super(jstr, new FPDBReplyInventory_Factory());
+    }
+}
+
+class FPDBReplyIOU extends FPDBReply<_FPDBReplyIOU>
+{
+    public FPDBReplyIOU(String jstr) throws FPDBException
+    {
+        super(jstr, new FPDBReplyIOU_Factory());
+    }
+}
+
+
 
 class FPDB
 {
-    private static JSONObject http_get(String url) throws FPDBException
+    private String url;
+    private String username;
+    private String password;
+
+    public FPDB(String url, String username, String password)
     {
-        String jstr = "";
-        JSONObject jobj;
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(url));
-            /*
-            URLConnection con = (new URL(url)).openConnection();
-            BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-            */
-            String line;
-            while ((line = br.readLine()) != null) {
-                jstr += line;
-            }
-            br.close();
-        } catch (IOException e) {
-            throw new FPDBException(e.getMessage());
-        }
-            
-        jobj = (JSONObject)JSONValue.parse(jstr);
-        if (jobj == null) {
-            throw new FPDBException("Parsing failed");
-        }
-
-        return jobj;
+        this.username = username;
+        this.password = password;
+        this.url = String.format("%s?username=%s&password=%s", url, username, password);
     }
 
-    public static FPDBReply request(String url) throws FPDBException
+    private String http_get_json(String url) throws IOException
     {
-        FPDBReply reply = new FPDBReply(http_get(url));
-        if (reply.type.equals("error")) {
-           throw new FPDBException(Integer.valueOf(reply.payload.get(0).get("code")),
-                                   reply.payload.get(0).get("message"));
+        URLConnection con = (new URL(url)).openConnection();
+        InputStreamReader sr = new InputStreamReader(con.getInputStream());
+        BufferedReader br = new BufferedReader(sr);
+
+        String line, jstr = "";
+        while ((line = br.readLine()) != null) {
+            jstr += line;
         }
-        return reply;
+
+        return jstr;
+    }
+
+    public FPDBReplyInventory inventory_get() throws FPDBException, IOException
+    {
+        String jstr = http_get_json(url + "&action=inventory_get");
+        return new FPDBReplyInventory(jstr);
+    }
+
+    public FPDBReplyIOU iou_get() throws FPDBException, IOException
+    {
+        String jstr = http_get_json(url + "&action=iou_get");
+        return new FPDBReplyIOU(jstr);
+    }
+
+    public FPDBReplyIOU iou_get_all() throws FPDBException, IOException
+    {
+        String jstr = http_get_json(url + "&action=iou_get_all");
+        return new FPDBReplyIOU(jstr);
     }
 }
+
 
--- api/FPDBCli.java
+++ api/FPDBCli.java
@@ -4,26 +4,25 @@ class FPDBCli
 {
     public static void main(String[] args)
     {
-        FPDBReply reply = null;
+        String url = "http://user.it.uu.se/~deklov/fpdb/api.php";
+        String username = "gurra";
+        String password = "gurra";
 
-        if (args.length != 1) {
-            System.out.println("Usage: FPDB_Cli url");
-            System.exit(-1);
-        }
-
+        FPDBReplyIOU reply = null;
         try {
-            reply = FPDB.request(args[0]);
-        } catch (FPDBException e) {
+            FPDB db = new FPDB(url, username, password);
+            reply = db.iou_get_all();
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(-1);
         }
 
-        System.out.println("type: " + reply.type);
-        for (Map<String, String> i : reply) {
-            System.out.println("  payload:");
-            for (Map.Entry<String, String> j : i.entrySet()) {
-                System.out.println("    " + j.getKey() + ": " + j.getValue());
-            }
+        System.out.println("IOU");
+        for (_FPDBReplyIOU i : reply.payload) {
+            System.out.println("username: " + i.username);
+            System.out.println("first_name: " + i.first_name);
+            System.out.println("last_name: " + i.last_name);
+            System.out.println("assets: " + i.assets);
         }
     }
 }
