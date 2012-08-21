17486e4bdc0ff0770f898a1a9fae019a7d1ae86b
David Eklov <david.eklov@it.uu.se>
2012-08-21 09:00:09 +0200

    Cleaned up error checking

--- api/FPDB.java
+++ api/FPDB.java
@@ -34,7 +34,7 @@ interface FPDBReplyFactory<T>
 
 class FPDBReply<T> implements Iterable<T>
 {
-    public LinkedList<T> payload;
+    private LinkedList<T> payload;
 
     public FPDBReply(String jstr, FPDBReplyFactory<T> factory) throws FPDBException
     {
@@ -47,30 +47,17 @@ class FPDBReply<T> implements Iterable<T>
 
             jarr = jobj.getJSONArray("payload");
             type = (String)jobj.get("type");
-        } catch (JSONException e) {
-            throw new FPDBException("JSON Error");
-        }
 
-        if (type == null) {
-            throw new FPDBException("JSON no type attribute");
-        }
-
-        if (type.equals("error")) {
-            throw new FPDBException("TODO: Get error message");
-        }
-        
-        if (jobj.isNull("payload")) {
-            throw new FPDBException("JSON no payload attribute");
-        }
-
-        payload = new LinkedList<T>();
-        try {
+            if (type.equals("error")) {
+                throw new FPDBException(jarr.getJSONObject(0).getString("msg"));
+            }
+            
+            payload = new LinkedList<T>();
             for (int i = 0; i < jarr.length(); i++) {
                 payload.add(factory.create(jarr.getJSONObject(i)));
             }
         } catch (JSONException e) {
-            throw new FPDBException("JSON Error");
-
+            throw new FPDBException(e.getMessage());
         }
     }
 
@@ -84,9 +71,9 @@ class FPDBReply<T> implements Iterable<T>
 class FPDBReplyInventory
 {
     public String name;
-    public int    beer_id;
-    public int    count;
-    public float  price;
+    public int beer_id;
+    public int count;
+    public float price;
 
     public FPDBReplyInventory(JSONObject jobj) throws JSONException
     {
@@ -112,7 +99,7 @@ class FPDBReplyIOU
     public String username;
     public String first_name;
     public String last_name;
-    public float  assets;
+    public float assets;
 
     public FPDBReplyIOU(JSONObject jobj) throws JSONException
     {
@@ -147,31 +134,43 @@ class FPDB
 
     private String http_get_json(String url) throws IOException
     {
-        URLConnection con = (new URL(url)).openConnection();
-        InputStreamReader sr = new InputStreamReader(con.getInputStream());
-        BufferedReader br = new BufferedReader(sr);
-
         String line, jstr = "";
-        while ((line = br.readLine()) != null) {
-            jstr += line;
+        URLConnection co;
+        InputStreamReader sr;
+        BufferedReader br = null;
+
+        try {
+            co = (new URL(url)).openConnection();
+            sr = new InputStreamReader(co.getInputStream());
+            br = new BufferedReader(sr);
+
+            while ((line = br.readLine()) != null) {
+                jstr += line;
+            }
+        } catch (IOException e) {
+            new FPDBException(e.getMessage());
+        } finally {
+            if (br != null) {
+                br.close();
+            }
         }
 
         return jstr;
     }
 
-    public FPDBReply<FPDBReplyInventory> inventory_get() throws FPDBException, IOException, JSONException
+    public FPDBReply<FPDBReplyInventory> inventory_get() throws FPDBException, IOException
     {
         String jstr = http_get_json(url + "&action=inventory_get");
         return new FPDBReply<FPDBReplyInventory>(jstr, new FPDBReplyInventory_Factory());
     }
 
-    public FPDBReply<FPDBReplyIOU> iou_get() throws FPDBException, IOException, JSONException
+    public FPDBReply<FPDBReplyIOU> iou_get() throws FPDBException, IOException
     {
         String jstr = http_get_json(url + "&action=iou_get");
         return new FPDBReply<FPDBReplyIOU>(jstr, new FPDBReplyIOU_Factory());
     }
 
-    public FPDBReply<FPDBReplyIOU> iou_get_all() throws FPDBException, IOException, JSONException
+    public FPDBReply<FPDBReplyIOU> iou_get_all() throws FPDBException, IOException
     {
         String jstr = http_get_json(url + "&action=iou_get_all");
         return new FPDBReply<FPDBReplyIOU>(jstr, new FPDBReplyIOU_Factory());
